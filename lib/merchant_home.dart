a507d7cfaab3009559dd476b45b13bd9451693e5
Devon Lin <44730387+Devon-Lin@users.noreply.github.com>
2022-10-16 13:44:03 -0400

    adding routing to mechant_receipt_history

--- lib/merchant_home.dart
+++ lib/merchant_home.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:ceg4912_project/item_page.dart';
 import 'package:ceg4912_project/Support/session.dart';
 import 'package:ceg4912_project/Support/utility.dart';
+import 'package:ceg4912_project/merchant_receipt_history.dart';
 import 'package:ceg4912_project/merchant_receipt_page.dart';
 import 'package:flutter/material.dart';
 import 'Models/user.dart';
@@ -39,7 +40,14 @@ class _MerchantHomePageState extends State<MerchantHomePage> {
     MerchantReceiptPage.receiptItems.clear();
   }
 
-  void loadReceiptHistoryPage() {}
+  void loadReceiptHistoryPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const MerchantReceiptHistoryPage(),
+      ),
+    );
+  }
 
   void loadMerchantAccountPage() {}
 
