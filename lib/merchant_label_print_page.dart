65767ca5d316bc7a45e2ac43e33ab5e494adc0fa
Bryse <brysonmacduff@gmail.com>
2022-10-28 19:42:57 -0400

    Added label printing. Needs testing with an actual printer

--- lib/merchant_label_print_page.dart
+++ lib/merchant_label_print_page.dart
@@ -2,7 +2,15 @@ import 'package:ceg4912_project/Support/utility.dart';
 import 'package:flutter/material.dart';
 import 'package:ceg4912_project/Models/item.dart';
 import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/services.dart';
 import 'package:zoom_widget/zoom_widget.dart';
+import 'package:barcode_image/barcode_image.dart';
+import 'package:barcode/barcode.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class MerchantLabelPrintPage extends StatefulWidget {
   const MerchantLabelPrintPage({Key? key}) : super(key: key);
@@ -15,8 +23,27 @@ class _MerchantLabelPrintPageState extends State<MerchantLabelPrintPage> {
   // stores the item for which a barcode label will be printed
   Item _item = Item.empty();
 
-  // TO DO - Add label printing
-  void printLabel() {}
+  void printLabel() async {
+    // get application document directory
+    Directory appDocDir = await getTemporaryDirectory();
+
+    // create an svg barcode image
+    final bc = Barcode.code128();
+    final svg =
+        bc.toSvg(_item.toLabelJSON(), width: 200, height: 200, fontHeight: 0);
+
+    final doc = pw.Document();
+    doc.addPage(pw.Page(build: (pw.Context context) {
+      return pw.Center(
+          child:
+              pw.SvgImage(svg: svg) //pw.Image.memory(imageBytes)//pw.Image(ip),
+          );
+    }));
+
+    // initiate the print job
+    await Printing.layoutPdf(
+        onLayout: (PdfPageFormat format) async => doc.save());
+  }
 
   @override
   Widget build(BuildContext context) {
