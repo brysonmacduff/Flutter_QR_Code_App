80f368b9fba6951eef4c0a9f0fd3f8c8319276ce
JustinLim44 <jlim017@uottawa.ca>
2022-11-03 11:32:11 -0400

    added dependancy back and functions to create customer

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:stripe_sdk/stripe_sdk.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,7 +13,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Map<String, dynamic>? paymentIntent;
-  
+  final client = http.Client();
+  static Map<String, String> headers = {
+    'Authorization':
+        'Bearer sk_test_51LTCRODkzVSkvB16MkkVIZ1UZl5ewJzmaB9Qgm9yQrE8jTWX8UjrM1L8cu4ty6BI2SSyLKgvxqXGK1UVANlUQyc500J8r4XRY1',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,14 +37,93 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+//new
+  Future<Map<String, dynamic>> createCustomer() async {
+    var response = await client.post(
+      Uri.parse('https://api.stripe.com/v1/customers'),
+      headers: headers,
+      body: {'description': 'new customer'},
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print(json.decode(response.body));
+      throw 'Failed to register as a customer.';
+    }
+  }
+
+//new method
+  Future<Map<String, dynamic>> createPaymentMethod(
+      {required String number,
+      required String expMonth,
+      required String expYear,
+      required String cvc}) async {
+    var response = await client.post(
+      Uri.parse('https://api.stripe.com/v1/payment_methods'),
+      headers: headers,
+      body: {
+        'type': 'card',
+        'card[number]': number,
+        'card[exp_month]': expMonth,
+        'card[exp_year]': expYear,
+        'card[cvc]': cvc,
+      },
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print(json.decode(response.body));
+      throw 'Failed to create PaymentMethod.';
+    }
+  }
+
+//new method
+  Future<Map<String, dynamic>> attachPaymentMethod(
+      String paymentMethodId, String customerId) async {
+    var response = await client.post(
+      Uri.parse(
+          'https://api.stripe.com/v1/payment_methods/$paymentMethodId/attach'),
+      headers: headers,
+      body: {
+        'customer': customerId,
+      },
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print(json.decode(response.body));
+      throw 'Failed to attach PaymentMethod.';
+    }
+  }
+
+//new method
+  Future<Map<String, dynamic>> updateCustomer(
+      String paymentMethodId, String customerId) async {
+    var response = await client.post(
+      Uri.parse('https://api.stripe.com/v1/customers/$customerId'),
+      headers: headers,
+      body: {
+        'invoice_settings[default_payment_method]': paymentMethodId,
+      },
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print(json.decode(response.body));
+      throw 'Failed to update Customer.';
+    }
+  }
+
   Future<void> makePayment() async {
     try {
+      final customer = await createCustomer();
       paymentIntent = await createPaymentIntent('15', 'CAD');
       //Payment Sheet
       await Stripe.instance
           .initPaymentSheet(
               paymentSheetParameters: SetupPaymentSheetParameters(
                   paymentIntentClientSecret: paymentIntent!['client_secret'],
+                  customerId: customer['id'],
                   style: ThemeMode.dark,
                   merchantDisplayName: 'Merchant'))
           .then((value) {});
