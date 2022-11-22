aae50912a12fcbd7a00c1e380f2f7fa30f276dd3
Bassel A <babou040@uottawa.ca>
2022-11-21 20:45:22 -0500

    Updated Customer sign-up Payment sheet to work on Physical Devices

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:ceg4912_project/login.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
 
-void main() {
+void main() async{
+  Stripe.merchantIdentifier = 'POPCode';
   Stripe.publishableKey =
   "pk_test_51LTCRODkzVSkvB16csikvssY9E1AVG5JXBSQYcpYEKcJuxHBumW0UZvNDn60fPIYMBEni8EtCmA9BVVfT5rUr3n800qu2KQAso";
+  await Stripe.instance.applySettings();
   runApp(const LogInPageRoute());
 }
